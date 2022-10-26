3c0f02da3adf050280e9812661d703d4c8b24960
imlovi <dianlovita08@gmail.com>
2022-10-26 11:40:59 +0700

    first up

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hey There!";
+
+    $nama = "<br> Ini aku";
+    $nama1 = "Lovi";
+    $umur = 21;
+
+    print "$nama <b> $nama1 </b> umur $umur tahun";
+
+    $namaKakak = "Prince";
+    $umurKakak = 27;
+
+    echo "<br> Aku punya kakak, namanya <b> $namaKakak </b> yang berumur $umurKakak tahun";
+
+    $perbedaan = $umurKakak - $umur;
+    echo "<br> selisih usia saya dan kakak adalah $perbedaan tahun."
+?>
