bdf5aba972aa18e30481b6861f0798b6166e8603
imlovi <dianlovita08@gmail.com>
2022-11-19 10:15:35 +0700

    add name nd address

--- form.php
+++ form.php
@@ -1,19 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learning Login</title>
-</head>
-<body>
-    <h1> Form Login </h1>
-    <form action= "action.php?p=pesan" method="post">
-        Nama :
-        <input type="text" name="nama">
-        Password :
-        <input type="password" name="password">
-        <input type="submit" name="submit" value="Submit">
-    </form>
-</body>
-</html>
+<title> Learning form </title>
+<form method="POST" action="" enctype="multipart/form-data">
+    <table> 
+    <tr>
+        <td> Nama </td>
+        <td> : </td>
+        <td> <input type="text" name="nama"> </td>
+    </tr>
+    <tr>
+        <td> Alamat </td>
+        <td> : </td>
+        <td> <input type="text" name="alamat"> </td>
+    </tr>
+    <tr>
+        <td> Foto </td>
+        <td> : </td>
+        <td> <input type="file" name="file"> </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td> <input type="Submit" name="hasil" value="SEND"> </td>
+    </tr>
+    </table>
+</form>
+
+<?php
+    if (isset($_POST["hasil"])) {
+
+        $nama = $_POST["nama"];
+        $alamat = $_POST["alamat"];
+
+        $file = $_FILES["file"] ["name"];
+        $tmp_name = $_FILES['file'] ["tmp_name"];
+
+        move_uploaded_file($tmp_name, "gambar/". $file);
+    }
+    ?>
+
+<table border="1" cellspacing="0">
+    <tr>
+        <td>Nama</td>
+        <td>Alamat</td>
+        <td>Foto</td>
+    </tr>
+    <tr>
+        <td><?php echo $nama ?></td>
+        <td><?php echo $alamat ?></td>
+        <td><img src="gambar/<?php echo $file ?>" style="width: 100px"></td>
+    </tr>
+</table>
